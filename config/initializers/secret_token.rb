5c121ff1f2684adb2d683d8b4952e1ef81ba9692
Necromos <masternecromos@gmail.com>
2012-11-15 18:31:42 +0100

    pierwszy commit apki

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainToster::Application.config.secret_token = '1d78c14b696b7a64fba55dba12fc36f0aabfdfd12880d0eb81eb16934d11a6dd811fe013bc818aa193066b2c1821e01ef550459cc4ce27225bc2c1ff8718073d'
