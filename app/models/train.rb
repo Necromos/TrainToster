abdbca475f20404c4ee2a2459f62b314be1edd93
Necromos <masternecromos@gmail.com>
2012-11-17 16:23:23 +0100

    poprawka walidatorów + view

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,5 @@
 class Train < ActiveRecord::Base
 	validates :number, :length => { :minimum => 3, :maximum => 6}, :uniqueness => true, :presence => true
-	validates :name, 
 	validates :from, :presence => true
 	validates :to, :presence => true
 	validates :departure, :presence => true
