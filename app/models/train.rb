1ee645eceef019f680bfedc3d5fb3f0eda8a0e70
Necromos <masternecromos@gmail.com>
2012-11-17 16:18:21 +0100

    dodaje walidatory

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,2 +1,9 @@
 class Train < ActiveRecord::Base
+	validates :number, :length => { :minimum => 3, :maximum => 6}, :uniqueness => true, :presence => true
+	validates :name, 
+	validates :from, :presence => true
+	validates :to, :presence => true
+	validates :departure, :presence => true
+	validates :arrival, :presence => true
+	validates :carrier, :presence => true
 end
